192a8e12c27ee59df290b891bc0f09e9fd5694bf
SANJAI-909 <sanjaisk83@gmail.com>
2024-08-21 06:25:16 +0000

    pattern j completed successfully

---
+++ some more imp patterns/pattern_j.cpp
@@ -0,0 +1,34 @@
+/*
+ j) *
+    **
+    ***
+    ****
+    *****
+    ****
+    ***
+    **
+    *
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+     for (int i = 1; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
